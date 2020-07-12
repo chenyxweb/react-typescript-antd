d01a43b1f95d1e0c8a71585e0f05f7312f9beb62
chenyxweb <chenyx_web@163.com>
2020-07-12 22:01:19 +0800

    rename

---
+++ vikingship/src/components/Icon/icon.tsx
@@ -0,0 +1,22 @@
+import React, { FC } from 'react'
+import { FontAwesomeIcon, FontAwesomeIconProps } from '@fortawesome/react-fontawesome'
+import classNames from 'classnames'
+
+export type ThemeProps = 'primary' | 'secondary' | 'success' | 'info' | 'warning' | 'danger' | 'light' | 'dark'
+
+// 继承 FontAwesomeIcon 的props
+interface IconProps extends FontAwesomeIconProps {
+  // 图标颜色
+  theme?: ThemeProps
+}
+
+const Icon: FC<IconProps> = props => {
+  const { className, theme, ...restProps } = props
+  const classes = classNames('viking-icon', className, {
+    [`icon-${theme}`]: theme,
+  })
+
+  return <FontAwesomeIcon className={classes} {...restProps} />
+}
+
+export default Icon
